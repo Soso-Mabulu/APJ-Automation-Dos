8366abdc1bde6ee8b570648e7d951f0582211fe2
Soso-Mabulu <112823408+Soso-Mabulu@users.noreply.github.com>
2023-06-01 23:39:54 +0200

    Learning and creating components

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+//Import the home component 
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Hello world!</h1>`,
+  imports: [
+    HomeComponent,
+  ],
+  //Setting the Brang logo and and header
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
