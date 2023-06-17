ac196792dcec99df45cef69f504fbd1f72b673ea
Soso-Mabulu <112823408+Soso-Mabulu@users.noreply.github.com>
2023-06-17 11:08:52 +0200

    Final housing project with Images and all assets intact
    This project is a housing site where clients can seach for a house they like and would wanna buy or rent. Can Search by filtering location and they can view more detail and book the house by clicking "learn more".

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -29,22 +29,22 @@ import { HousingService } from '../housing.service';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
-housingService: HousingService = inject(HousingService);
-filteredLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
-constructor() {
-  this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
-    this.housingLocationList = housingLocationList;
-    this.filteredLocationList = housingLocationList;
-  });
-}
-filterResults(text: string) {
-  if (!text) {
-    this.filteredLocationList = this.housingLocationList;
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
 
-  this.filteredLocationList = this.housingLocationList.filter(
-    housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
-  );
-};
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  };
 }
--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
-import { RouterModule, RouterLink , RouterOutlet } from '@angular/router';
+import { RouterLink , RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
